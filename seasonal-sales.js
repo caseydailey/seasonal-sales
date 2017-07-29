8752ab957b0ecb3290b7a4a68a4898f02c5a9efd
Casey Dailey <casey@feasttogether.org>
2017-07-29 13:53:20 -0500

    boiler plate set. xhr set. logging data.

---
+++ seasonal-sales.js
@@ -0,0 +1,25 @@
+"use strict";
+console.log("whaddup");
+
+
+function get(file) {
+    console.log("get fired" );
+    let xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", display);
+    xhr.addEventListener("error", oops);
+    xhr.open("GET", file);
+    xhr.send();
+
+}
+
+function display(load){
+    let data = JSON.parse(this.responseText);
+    console.log("load data: ", data);
+}
+
+function oops(error){
+    console.log("there was an error with the hxr", error);
+}
+
+get('products.json');
+get('categories.json');
