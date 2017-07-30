99651e08a3a5b5f232719b4b4453ef762f3931f9
Casey Dailey <casey@feasttogether.org>
2017-07-30 12:17:25 -0500

    adding category info to products

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -1,24 +1,48 @@
 "use strict";
 
+const display = document.querySelector("#display");
+let products = [];
+let categories = [];
 
 function get(file) {
-    console.log("get fired" );
     let xhr = new XMLHttpRequest();
-    xhr.addEventListener("load", display);
+    xhr.addEventListener("load", store);
     xhr.addEventListener("error", oops);
     xhr.open("GET", file);
     xhr.send();
 
 }
 
-function display(load){
-    let data = JSON.parse(this.responseText);
-    console.log(`load data ${Object.keys(data)[0]}: `, data);
-}
-
 function oops(error){
     console.log("there was an error with the hxr", error);
 }
+
+function store(load){
+    let data = JSON.parse(this.responseText);
+    let value = Object.keys(data)[0];
+    switch(value){
+        case 'products': products = data.products;
+        console.log("products", products);
+        break;
+        case 'categories': categories = data.categories;
+        console.log("categories", categories);
+        break;
+    }
+    combine(products, categories);
+}
+
+function combine(prod, cat){
+    prod.forEach((product)=>{
+        cat.forEach((category)=>{
+            if(product.category_id === category.id){
+                product.category_info = category;
+            }
+        });
+    });
+console.log("products with cat_info: ", products);
+}
+
+
 
 get('products.json');
 get('categories.json');
